d8905c319cd338516d68df889d52de17e56fc884
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-09-21 23:12:14 +0200

    drivers: input: stm: fix build errors
    Co-authored-by: Nahuel Gómez <nahuelgomez329@gmail.com>
    Signed-off-by: Salvo Giangreco <giangrecosalvo9@gmail.com>

--- drivers/input/sec_input/stm/stm_cmd.c
+++ drivers/input/sec_input/stm/stm_cmd.c
@@ -950,22 +950,22 @@ void stm_ts_print_frame(struct stm_ts_data *ts, short *min, short *max)
 	if (pStr == NULL)
 		return;
 
-	snprintf(pTmp, 4, "    ");
+	snprintf(pTmp, 5, "    ");
 	strlcat(pStr, pTmp, lsize);
 
 	for (i = 0; i < ts->rx_count; i++) {
-		snprintf(pTmp, 6, "Rx%02d  ", i);
+		snprintf(pTmp, 7, "Rx%02d  ", i);
 		strlcat(pStr, pTmp, lsize);
 	}
 
 	input_raw_info_d(true, &ts->client->dev, "%s\n", pStr);
 
 	memset(pStr, 0x0, 6 * (ts->rx_count + 1));
-	snprintf(pTmp, 2, " +");
+	snprintf(pTmp, 3, " +");
 	strlcat(pStr, pTmp, lsize);
 
 	for (i = 0; i < ts->rx_count; i++) {
-		snprintf(pTmp, 6, "------");
+		snprintf(pTmp, 7, "------");
 		strlcat(pStr, pTmp, lsize);
 	}
 
@@ -973,11 +973,11 @@ void stm_ts_print_frame(struct stm_ts_data *ts, short *min, short *max)
 
 	for (i = 0; i < ts->tx_count; i++) {
 		memset(pStr, 0x0, 6 * (ts->rx_count + 1));
-		snprintf(pTmp, 7, "Tx%02d | ", i);
+		snprintf(pTmp, 8, "Tx%02d | ", i);
 		strlcat(pStr, pTmp, lsize);
 
 		for (j = 0; j < ts->rx_count; j++) {
-			snprintf(pTmp, 6, "%5d ", ts->pFrame[(i * ts->rx_count) + j]);
+			snprintf(pTmp, 7, "%5d ", ts->pFrame[(i * ts->rx_count) + j]);
 			strlcat(pStr, pTmp, lsize);
 
 			if (ts->pFrame[(i * ts->rx_count) + j] < *min)
@@ -1613,18 +1613,18 @@ int stm_ts_panel_test_result(struct stm_ts_data *ts, int type)
 	stm_ts_read_nonsync_frame(ts, &temp_min, &temp_max);
 
 	if (type == OPEN_TEST && result == -ITO_FAIL_OPEN) {
-		snprintf(result_buff, sizeof(result_buff), "NG");
-		sec_cmd_set_cmd_result(sec, result_buff, strnlen(result_buff, sizeof(result_buff)));
+		snprintf(result_buff, size, "NG");
+		sec_cmd_set_cmd_result(sec, result_buff, strnlen(result_buff, size));
 		sec->cmd_state = SEC_CMD_STATUS_FAIL;
 
 	} else if (type == SHORT_TEST && result == -ITO_FAIL_SHORT) {
-		snprintf(result_buff, sizeof(result_buff), "NG");
-		sec_cmd_set_cmd_result(sec, result_buff, strnlen(result_buff, sizeof(result_buff)));
+		snprintf(result_buff, size, "NG");
+		sec_cmd_set_cmd_result(sec, result_buff, strnlen(result_buff, size));
 		sec->cmd_state = SEC_CMD_STATUS_FAIL;
 
 	} else {
-		snprintf(result_buff, sizeof(result_buff), "OK");
-		sec_cmd_set_cmd_result(sec, result_buff, strnlen(result_buff, sizeof(result_buff)));
+		snprintf(result_buff, size, "OK");
+		sec_cmd_set_cmd_result(sec, result_buff, strnlen(result_buff, size));
 		sec->cmd_state = SEC_CMD_STATUS_OK;
 	}
 
