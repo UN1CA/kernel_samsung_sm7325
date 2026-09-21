60e7d039d8d44257e24e2e7ae917bd31ec7f6721
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-09-21 22:44:28 +0200

    fs: ksmbd: import S901EXXSEGZE3 changes
    Signed-off-by: Salvo Giangreco <giangrecosalvo9@gmail.com>

--- fs/ksmbd/smb2pdu.c
+++ fs/ksmbd/smb2pdu.c
@@ -18,6 +18,7 @@
 #include <linux/filelock.h>
 #endif
 
+#include<linux/string.h>
 #include "compat.h"
 #include "glob.h"
 #include "smb2pdu.h"
@@ -44,6 +45,51 @@
 #include "mgmt/user_session.h"
 #include "mgmt/ksmbd_ida.h"
 #include "ndr.h"
+
+/**
+ * Function to get filename or folder name
+ * from absolute path
+ */
+const char *get_extract(const char *name)
+{
+	const char *last_slash = NULL;
+
+	last_slash = strrchr(name, '/');
+
+	if (last_slash != NULL)
+		return last_slash+1;
+
+	return name;
+}
+
+/**
+ * Function for returning last three characters of string name
+ * return string with last three characters
+ */
+const char *get_last_three_chars(const char *name)
+{
+	int len = strlen(name);
+
+	if (len <= 3)
+		return &name[len];
+
+	return &name[len-3];
+}
+
+/**
+ * Function for returning the filename from File object
+ */
+const char *get_filename(struct file *filp)
+{
+	struct dentry *dentry;
+	const char *filename = NULL;
+
+	if (!filp || !filp->f_path.dentry)
+		return NULL;
+	dentry = filp->f_path.dentry;
+	filename = (const char *)dentry->d_name.name;
+	return filename;
+}
 
 static void __wbuf(struct ksmbd_work *work, void **req, void **rsp)
 {
@@ -2957,7 +3003,8 @@ int smb2_open(struct ksmbd_work *work)
 			goto err_out2;
 		}
 
-		ksmbd_debug(SMB, "converted name = %s\n", name);
+		ksmbd_debug(SMB, "converted name = *%s\n",
+			    get_last_three_chars(get_extract((const char *)name)));
 		if (strchr(name, ':')) {
 			if (!test_share_config_flag(work->tcon->share_conf,
 						    KSMBD_SHARE_FLAG_STREAMS)) {
@@ -3213,8 +3260,8 @@ int smb2_open(struct ksmbd_work *work)
 	} else {
 		if (rc != -ENOENT)
 			goto err_out;
-		ksmbd_debug(SMB, "can not get linux path for %s, rc = %d\n",
-			    name, rc);
+		ksmbd_debug(SMB, "can not get linux path for *%s, rc = %d\n",
+			    get_last_three_chars(get_extract((const char *)name)), rc);
 		rc = 0;
 	}
 
@@ -3306,6 +3353,12 @@ int smb2_open(struct ksmbd_work *work)
 			rc = -EACCES;
 			goto err_out;
 		}
+	}
+
+	if (req->CreateOptions & FILE_DIRECTORY_FILE_LE ||
+			(file_present && S_ISDIR(d_inode(path.dentry)->i_mode))) {
+		open_flags &= ~O_ACCMODE;
+		may_flags &= ~MAY_WRITE;
 	}
 
 	/*create file if not present */
@@ -5883,7 +5936,6 @@ int smb2_query_info(struct ksmbd_work *work)
 	WORK_BUFFERS(work, req, rsp);
 
 	ksmbd_debug(SMB, "GOT query info request\n");
-
 	if (ksmbd_override_fsids(work)) {
 		rc = -ENOMEM;
 		goto err_out;
@@ -6166,7 +6218,8 @@ static int smb2_rename(struct ksmbd_work *work,
 		goto out;
 	}
 
-	ksmbd_debug(SMB, "new name %s\n", new_name);
+	ksmbd_debug(SMB, "new name *%s\n",
+		    get_last_three_chars(get_extract((const char *)new_name)));
 	if (ksmbd_share_veto_filename(share, new_name)) {
 		rc = -ENOENT;
 		ksmbd_debug(SMB, "Can't rename vetoed file: %s\n", new_name);
@@ -6271,7 +6324,8 @@ static int smb2_rename(struct ksmbd_work *work,
 		goto out;
 	}
 
-	ksmbd_debug(SMB, "new name %s\n", new_name);
+	ksmbd_debug(SMB, "new name *%s\n",
+		    get_last_three_chars(get_extract((const char *)new_name)));
 	rc = ksmbd_vfs_kern_path(work, new_name, LOOKUP_NO_SYMLINKS, &path, 1);
 	if (rc) {
 		if (rc != -ENOENT)
@@ -7181,8 +7235,8 @@ int smb2_read(struct ksmbd_work *work)
 		goto out;
 	}
 
-	ksmbd_debug(SMB, "filename %pD, offset %lld, len %zu\n",
-		    fp->filp, offset, length);
+	ksmbd_debug(SMB, "filename *%s, offset %lld, len %zu\n",
+		    get_last_three_chars(get_filename(fp->filp)), offset, length);
 
 	aux_payload_buf = kvzalloc(length, GFP_KERNEL);
 	if (!aux_payload_buf) {
@@ -7447,8 +7501,8 @@ int smb2_write(struct ksmbd_work *work)
 		data_buf = (char *)(((char *)&req->hdr.ProtocolId) +
 				    le16_to_cpu(req->DataOffset));
 
-		ksmbd_debug(SMB, "filename %pD, offset %lld, len %zu\n",
-			    fp->filp, offset, length);
+		ksmbd_debug(SMB, "filename *%s, offset %lld, len %zu\n",
+			    get_last_three_chars(get_filename(fp->filp)), offset, length);
 		err = ksmbd_vfs_write(work, fp, data_buf, length, &offset,
 				      writethrough, &nbytes);
 		if (err < 0)
